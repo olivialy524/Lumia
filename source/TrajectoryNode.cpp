bb4fa1b57746dbff1cf4d59f7e142b019a73ffa0
Vivi Ye <viviyyw0418@gmail.com>
2021-04-11 22:36:50 -0400

    add files

---
+++ source/TrajectoryNode.cpp
@@ -0,0 +1,9 @@
+//
+//  TrajectoryNode.cpp
+//  Lumia
+//
+//  Created by Vivi Ye on 4/9/21.
+//  Copyright © 2021 Cornell Game Design Initiative. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ source/TrajectoryNode.h
@@ -0,0 +1,33 @@
+//
+//  TrajectoryNode.h
+//  Lumia
+//
+//  Created by Vivi Ye on 4/9/21.
+//  Copyright © 2021 Cornell Game Design Initiative. All rights reserved.
+//
+
+#ifndef TrajectoryNode_h
+#define TrajectoryNode_h
+
+#include <string>
+#include <cugl/cugl.h>
+
+using namespace cugl
+
+class TrajectoryNode : public scene2::TextureNode {
+    
+    
+protected:
+   
+    
+
+public:
+    ~TrajectoryNode() { dispose(); }
+    
+    TrjectoryNode(): TextureNode(){}
+    
+    
+    
+
+}
+  
