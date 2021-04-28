f0c886e2a56c85f365d20118f58feae0c6729134
olivialy524 <olivialy524@gmail.com>
2021-04-27 22:08:50 -0400

    remove excess code

--- source/TileModel.cpp
+++ source/TileModel.cpp
@@ -19,16 +19,6 @@
 #define DEBUG_COLOR     Color4::YELLOW
 
 void TileModel::setTextures(const std::shared_ptr<Texture>& image){
-
-//    _sceneNode = scene2::SceneNode::allocWithBounds(Size(getWidth()*_drawscale,getHeight()*_drawscale));
-//    _sceneNode->setAnchor(Vec2::ANCHOR_CENTER);
-//    _sceneNode->setAngle(getAngle());
-//    _node = StickyWallNode::alloc(texture, 1, 1, 1);
-//    _node->setPolygon(Rect(getX()*_drawscale, getY()*_drawscale, getWidth()*_drawscale, getHeight()*_drawscale));
-//    _node->setAnchor(Vec2::ZERO);
-//    _node->setFrame(0);
-//    _sceneNode->addChild(_node);
-    
     auto platform = getPolygon() * _drawScale;
 
     // calcuate the drawing overlay scale
@@ -38,12 +28,6 @@ void TileModel::setTextures(const std::shared_ptr<Texture>& image){
     _node = scene2::PolygonNode::allocWithTexture(image);
     _node->setScale(Vec2(scalex, scaley));
     _node->setAngle(getAngle());
-   
-//    _world->addObstacle(platobj);
-//    tileobj->setDebugScene(_debugnode);
-//    tileobj->setPosition(t->getX(), t->getY());
-    
-//    tileobj->setType(t->getType());
     _node->setPosition(getPosition()*_drawScale);
 }
 
