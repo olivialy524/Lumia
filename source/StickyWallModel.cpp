aa6384d97b55ce5a9a73b0d367c0295468efeebe
olivialy524 <olivialy524@gmail.com>
2021-05-10 21:09:39 -0400

    add animation for sticky

--- source/StickyWallModel.cpp
+++ source/StickyWallModel.cpp
@@ -11,13 +11,15 @@
 
 void StickyWallModel::setTextures(const std::shared_ptr<Texture>& texture){
     
-    _sceneNode = scene2::SceneNode::allocWithBounds(Size(getWidth()*_drawscale/5.0f * 2.0f,getHeight()*_drawscale));
+    _sceneNode = scene2::SceneNode::allocWithBounds(Size(getWidth()*_drawscale,getHeight()*_drawscale));
     _sceneNode->setAnchor(Vec2::ANCHOR_CENTER);
     _sceneNode->setAngle(getAngle());
-    float scale = getWidth()/(texture->getWidth()/5.0f/_drawscale) * 2.0f;
+    float scale = getWidth() * _drawscale/(texture->getWidth()/5.0f);
+    float scale2 = 1.0/scale;
     _node = StickyWallNode::alloc(texture, 1, 5, 5);
+
+    _node->setPolygon(Rect(getX()*_drawscale, getY()*_drawscale, texture->getWidth()/5.0f, getHeight()*_drawscale *scale2));
     _node->setScale(scale);
-    _node->setPolygon(Rect(getX()*_drawscale, getY()*_drawscale, getWidth()*_drawscale * 2.0f, getHeight()*_drawscale));
     _node->setAnchor(Vec2::ZERO);
     _node->setFrame(0);
     _sceneNode->addChild(_node);
